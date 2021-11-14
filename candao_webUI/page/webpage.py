da3dac84765362fe22b0018c66477583329cac54
460850601 <460850601@qq.com>
2021-11-14 21:26:44 +0800

    调用浏览器为谷歌时自动更新驱动到驱动文件夹

--- candao_webUI/page/webpage.py
+++ candao_webUI/page/webpage.py
@@ -27,7 +27,7 @@ sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 
 from config.conf import cm
 from utils.times import sleep
-from utils import logger
+from utils import loggers
 
 
 log = log
@@ -52,7 +52,6 @@ class WebPage(object):
         try:
             self.driver.get(url)
             self.driver.implicitly_wait(8)
-            logger.info("打开网页：%s" % url)
             log.info("打开禅道主页：%s" % url)
         except TimeoutException:
             raise TimeoutException("打开%s超时请检查网络或网址服务器" % url)
@@ -70,7 +69,7 @@ class WebPage(object):
 
     def get_attrib(self, locator, value):
         """获取元素属性"""
-        logger.info("获取属性")
+        log.info("获取属性")
         ele = self.find_element(locator)
         sleep(0.5)
         return ele.get_attribute(value)
@@ -88,7 +87,7 @@ class WebPage(object):
     def elements_num(self, locator):
         """获取相同元素的个数"""
         number = len(self.find_elements(locator))
-        logger.info("相同元素：{}".format((locator, number)))
+        log.info("相同元素：{}".format((locator, number)))
         return number
 
     def input_text(self, locator, txt):
@@ -98,7 +97,7 @@ class WebPage(object):
         ele.clear()
         ele.send_keys(txt)
         sleep(0.5)
-        logger.info("输入文本：{}".format(txt))
+        log.info("输入文本：{}".format(txt))
 
     def input_enter(self, locator):
         """回车、tab等键入"""
@@ -110,12 +109,12 @@ class WebPage(object):
         """点击"""
         self.find_element(locator).click()
         sleep()
-        logger.info("点击元素：{}".format(locator))
+        log.info("点击元素：{}".format(locator))
 
     def element_text(self, locator):
         """获取当前的text"""
         _text = self.find_element(locator).text
-        logger.info("获取文本：{}".format(_text))
+        log.info("获取文本：{}".format(_text))
         return _text
 
     def hold_on(self, locator):
@@ -124,7 +123,7 @@ class WebPage(object):
         # 对定位到的元素执行悬停操作
         ActionChains(self.driver).move_to_element(move).perform()
         sleep()
-        logger.info("悬停元素：{}".format(locator))
+        log.info("悬停元素：{}".format(locator))
 
     def screen_scoll(self):
         self.driver.execute_script('window.scrollBy(0, 300)')
